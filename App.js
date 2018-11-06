81c2cc04c383ccc4632c8ba76529bfdace23c008
ctima <ctima@MacBook-Air-de-ctima.local>
2018-11-06 17:30:22 -0200

    fixed

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import  ResideMenu  from  'react-native-reside-menu';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <ResideMenu
+          onResideStateChange={(s) => { console.log(s) }}
+          VisibleComponent={() =>  <View style={{ flex: 1, backgroundColor: '#cdcdcd' }}  />} 								 
+          HiddenComponent={() =>  <View style={{ flex: 1, backgroundColor: 'aqua' }}  />}
+        />
       </View>
     );
   }
@@ -14,7 +19,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
